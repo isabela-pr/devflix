d21e338b3142ca38f3614d7e95097e7a89b34a4b
isabela-pr <icerqueira440@gmail.com>
2023-08-24 16:21:07 -0300

    projeto finalizado

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,11 +1,26 @@
-import styles from "./footer.module.css"
+// para utilizar o componente crie o import na página que deseja
+// coloque o nome da pessoa entre <Footer>NomePessoa</Footer>
+// passe a informação dentro da tag Footer - LINK
+// <Footer link()>isabela-pr</Footer>
+import SocialLinks from "../socialLinks/socialLinks";
+import "./footer.module.css";
+import "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
 
-const Footer = () => {
-    return(
-        <footer>
-
-        </footer>
-    )
+const Footer = ({ children, link }) => {
+  return (
+    <footer>
+      <p>
+        Feito com <ion-icon name="heart" /> por{" "}
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          {children}
+        </a>
+      </p>
+        <SocialLinks icon={"logo-github"} href={"#"} />
+        <SocialLinks icon={"logo-instagram"} href={"#"} />
+        <SocialLinks icon={"logo-twitch"} href={"#"} />
+        <SocialLinks icon={"logo-twitter"} href={"#"} />
+    </footer>
+  );
 };
 
 export default Footer;
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -6,6 +6,7 @@ import searchIcon from "../assets/search.svg";
 
 import "./App.css";
 import MovieCard from "../components/movieCard/movieCard";
+import Footer from "../components/footer/footer";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -60,6 +61,8 @@ const App = () => {
           <h2>Nenhum filme encontrado 😖</h2>
         </div>
       )}
+      
+      <Footer link={"https:github.com.br"}>isabela-pr</Footer>
     </div>
   );
 };
