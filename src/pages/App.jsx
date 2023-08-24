372d224f60350a7740dd5aa7ed415a5e7afd543b
isabela-pr <icerqueira440@gmail.com>
2023-08-24 16:41:31 -0300

    troquei chave de Api

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
 
-  const apiKey = "e4d577fa";
+  const apiKey = "b95efe03";
   const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
 
   useEffect(() => {
